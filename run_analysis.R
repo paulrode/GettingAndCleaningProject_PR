0cfc6494336cc140dd1021142b7355a81cad32b5
paulrode <paulrode@outlook.com>
2020-07-11 12:51:58 -0400

    Completed code

--- run_analysis.R
+++ run_analysis.R
@@ -2,15 +2,25 @@ source("setup.r")
 
 # Set up my R enviroment
 setup()
-ReadInFiles()
 
 
-# Merges the training and the test sets to create one data set.
+subject_test <- readLines("./data/subject_test.txt")
+subject_train <- readLines("./data/subject_train.txt")
+X_train   <-  read.table("./data/X_train.txt")
+y_train  <-  read.table("./data/y_train.txt")
+X_test   <-  read.table("./data/X_test.txt")
+y_test   <-  read.table("./data/y_test.txt")
+features  <-  read.table("./data/features.txt")
+activities <- read.table("./data/activity_labels.txt")
 
+# Merges the training and tes to create totalDate
+# First puts in discriptive feaure, activity, and subject names. 
+# Then combines the modified training and test data sets 
+# Cleans up feature names 
 
 colnames(activities) <- c("Activity", "Description")
-colnames(y_test) <- c("Activity", "Description")
-colnames(y_train) <- c("Activity", "Description")
+colnames(y_test) <- c("Activity")
+colnames(y_train) <- c("Activity")
 subject_test <- data.frame(Subject = subject_test)
 subject_train <- data.frame(Subject = subject_train)
 y_test <- left_join(y_test, activities)
@@ -18,19 +28,21 @@ y_train <- left_join(y_train, activities)
 test <- bind_cols(y_test, subject_test, X_test)
 train <- bind_cols(y_train, subject_train, X_train)
 totalData <-bind_rows(train, test)
-
-# Create feature names for totalData dataframe and clean up special characters 
+glimpse(totalData)
 featureNames <- c("Activity", "Description", "Subject", as.character(features[, 2]))
-
 featureNames <- gsub("^[t]|[f]", "", featureNames)
 featureNames = gsub("[()-]"," ",featureNames)
 colnames(totalData) <- featureNames
 subgroup <- grep("Mean|mean|std", featureNames, value = FALSE)
 
 sub_table <- totalData[,c(2,3,subgroup)]
-sub_table$Description <-as.character(sub_table$Description)
+#sub_table$Description <-as.character(sub_table$Description)
 sub_table$Subject <- as.numeric(sub_table$Subject)
+sub_table$Subject <- as.factor(sub_table$Subject)
+glimpse(sub_table)
 
-sub_table %>% group_by(Description, Subject) %>% summarize(funs(mean)) -> answer
 
-view(answer)
+sub_table %>% group_by(Subject, Description) %>% summarise_all(funs(mean)) -> answer
+               
+view(arrange(answer))
+
