e11d0809a3697baf18f3a1ad26e606e93a4c0586
paulrode <paulrode@outlook.com>
2020-07-10 21:59:26 -0400

    working of groupping and summerising

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ You should create one R script called run_analysis.R that does the following.
 
   1.  Merges the training and the test sets to create one data set.
   2.  Extracts only the measurements on the mean and standard deviation for each measurement.
-  3.  Uses descriptive activity names to name the activities in the data set
+  3.  Uses descriptive activity names to name the activities in the data set.
   4.  Appropriately labels the data set with descriptive variable names.
+  5.  From the data set in step 4, creates a second, independent tidy data set with the average of each            variable for each activity and each subject.
 
 The logic flow proceeds by first applying feature names and observation names to the test and training data sets. Next the two sets are combined to a master set as a tibble titled totalData. Finaly the feature headings are copied into a vector from which a grep() function is used to tag the mean and std feachers. Using this a select function is used to creat the std and mean database subset. 
 
 
-From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
--- run_analysis.R
+++ run_analysis.R
@@ -11,20 +11,26 @@ ReadInFiles()
 colnames(activities) <- c("Activity", "Description")
 colnames(y_test) <- c("Activity", "Description")
 colnames(y_train) <- c("Activity", "Description")
+subject_test <- data.frame(Subject = subject_test)
+subject_train <- data.frame(Subject = subject_train)
 y_test <- left_join(y_test, activities)
 y_train <- left_join(y_train, activities)
-test <- bind_cols(y_test, X_test)
-train <- bind_cols(y_train, X_train)
+test <- bind_cols(y_test, subject_test, X_test)
+train <- bind_cols(y_train, subject_train, X_train)
 totalData <-bind_rows(train, test)
 
 # Create feature names for totalData dataframe and clean up special characters 
-featureNames <- c("activity", "description", as.character(features[, 2]))
+featureNames <- c("Activity", "Description", "Subject", as.character(features[, 2]))
 
 featureNames <- gsub("^[t]|[f]", "", featureNames)
 featureNames = gsub("[()-]"," ",featureNames)
 colnames(totalData) <- featureNames
-grep("[Mean]|[mean]|[std]", featureNames, value = FALSE)
+subgroup <- grep("Mean|mean|std", featureNames, value = FALSE)
 
-view(totalData)
+sub_table <- totalData[,c(2,3,subgroup)]
+sub_table$Description <-as.character(sub_table$Description)
+sub_table$Subject <- as.numeric(sub_table$Subject)
 
+sub_table %>% group_by(Description, Subject) %>% summarize(funs(mean)) -> answer
 
+view(answer)
